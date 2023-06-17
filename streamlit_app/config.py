890f4215f08d60d5e4bb6903ec1f2416af94d757
PhuongAnhPhamNgoc <21520585@gm.uit.edu.vn>
2023-06-17 14:44:52 +0700

    upload

---
+++ streamlit_app/config.py
@@ -0,0 +1,20 @@
+datafields = "data/datafields.csv"
+
+nav_image = "data/Logo_UIT.jpg"
+home_image = "data/home.jpg"
+
+quarter_1 = "data/q1.csv"
+quarter_2 = "data/q2.csv"
+quarter_3 = "data/q3.csv"
+quarter_4 = "data/q4.csv"
+
+quarter_1_datasales = "data/q1_datesales.csv"
+quarter_2_datasales = "data/q2_datesales.csv"
+quarter_3_datasales = "data/q3_datesales.csv"
+quarter_4_datasales = "data/q4_datesales.csv"
+
+features = ["DayOfWeek","Customers", "Promo", "StateHoliday", "SchoolHoliday", "StoreType", "Assortment", "CompetitionDistance", 
+            "CompetitionOpenSinceMonth","Promo2SinceWeek", "PromoInterval", "MonthName"]
+salesDependingFeatures = [3,319,0,0,0,3,2,5350.00,4.00,22.00,2,6]
+model_load = "model/sales_prediction.sav"
+model_DL = "model/model.h5"
---
+++ streamlit_app/model.py
@@ -0,0 +1,67 @@
+import streamlit as st
+import pandas as pd
+import pickle
+# import tensorflow as tf
+from config import *
+from tensorflow import keras
+from keras.models import load_model
+#feature update
+def feature_update(li,features):
+    final_features = []
+    for i in li:
+        for j in features:
+         if(i == j):
+                final_features.append(features.index(j))
+    return final_features
+
+#page functioning
+def app():
+    html1 = '''
+                <style>
+                #heading{
+                  color: #E65142;
+                  text-align:top-left;
+                  font-size: 45px;
+                }
+                </style>
+                <h1 id = "heading"> Sales Data Prediction</h1>
+            '''
+    st.markdown(html1, unsafe_allow_html = True)
+
+
+    li = st.multiselect("Select the feature/features whose value can be manually updated ",features)
+    list = feature_update(li,features)
+    value = []
+    for i in list:
+        number = st.number_input("Enter the values " +features[i])
+        value.append(number)
+
+    for i in range(len(list)):
+        salesDependingFeatures[list[i]] = value[i]
+    d ={"Feature ":features, "Value for Prediction": salesDependingFeatures}
+    st.subheader("Default Values")
+    st.write(pd.DataFrame(d))
+
+    
+    loaded_model = pickle.load(open(model_load, 'rb'))
+    prediction_data = pd.DataFrame([salesDependingFeatures],
+                               columns=features)
+    
+    model_ = loaded_model.predict(prediction_data).flatten()
+    loaded_model_DL = keras.models.load_model(model_DL, compile=False).predict(prediction_data).flatten()
+    # st.subheader("The Predicted Value - XGBRegressor")
+    # st.write(model_)
+    # st.subheader("The Predicted Value - Deep Learning Model")
+    # st.write(loaded_model_DL)
+    # Create a dictionary to store the predictions
+
+    d = {
+        "XGBRegressor Predicted Value": model_,
+        "Deep Learning Predicted Value": loaded_model_DL
+    }
+
+    # Create a DataFrame from the dictionary
+    predictions_df = pd.DataFrame(d)
+
+    # Display the predictions in a table
+    st.write(predictions_df)
